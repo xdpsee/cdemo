51ef9055d2c14eba38152f7356520351e2047971
zhenhui.czh <zhenhui.czh@alibaba-inc.com>
2020-06-03 21:07:25 +0800

    modify...

--- cdemo/main.cpp
+++ cdemo/main.cpp
@@ -41,15 +41,15 @@ int main(int argc, const char *argv[]) {
 
     MusicPlayer *player = new MusicPlayer(NULL);
     MediaCollection *mediaCollection = player->queue();
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/周华健 - 迷途.mp3"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/周深 - 随风.ape"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/za.flac"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/pfzl.ape"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/bbb.mp4"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/张芸京-怎么哭.flac"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/杰文.mp3"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/Walking in the Cold.m4a"));
-    mediaCollection->addMedia(new MediaItem("/Users/zhenhui/audio_test/黄致列-苦海Live.ape"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/周华健 - 迷途.mp3"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/周深 - 随风.ape"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/za.flac"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/pfzl.ape"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/bbb.mp4"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/张芸京-怎么哭.flac"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/杰文.mp3"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/Walking in the Cold.m4a"));
+    mediaCollection->addMedia(new MediaItem("/Users/audio_test/黄致列-苦海Live.ape"));
 
     if (!player->open(mediaCollection->next(), true, true)) {
         std::cout << "open media item failed!" << std::endl;
